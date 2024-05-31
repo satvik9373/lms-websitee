d584532cb39e97c4c8d3664edb37fc494099b15b
satvik9373 <113889674+satvik9373@users.noreply.github.com>
2024-05-31 22:16:50 +0530

    ss

--- app/(router)/courses/_components/WelcomeBanner.jsx
+++ app/(router)/courses/_components/WelcomeBanner.jsx
@@ -8,7 +8,7 @@ function WelcomeBanner() {
         width={150}
         height={150}/>
         <div>
-            <h2  className='font-bold text-[27px]'>Welcome to <span className='text-primary'>Hustlers27</span> Academy</h2>
+            <h2  className='font-bold text-[27px]'>Welcome to <span className='text-primary'>Hustlers27</span> </h2>
             <h2 className='text-gray-500 '>Explore, learn and Earn real life projects experiences</h2>
         </div>
     </div>
